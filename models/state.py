74f7ca81da4cc3469e1542f4cb794c6498c5b6ef
Ahmedadel200066 <medo.adel200@gmail.com>
2023-12-05 18:13:42 +0200

    Add cities relationship to State model

--- models/state.py
+++ models/state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
      __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
